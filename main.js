e6a8fd5e85957adbd498a06a7ed7bdf43b68d336
vaibhav196606 <vaibhav199606@gmail.com>
2024-01-21 23:34:05 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const connectDb = require("./config/dbConnections");
+const dotenv = require("dotenv").config();
+const session = require("express-session");
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+connectDb();
+
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+    session({
+        secret : "my secret key",
+        saveUninitialized : true,
+        resave: false,
+    })
+);
+app.use((req,res,next)=>{
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+});
+app.use(express.static('uploads'));
+
+app.set("view engine","ejs");
+
+app.use("",require("./routes/routes.js"));
+
+app.listen(4000, ()=>{
+    console.log(`App is up and live on Port : ${port}`);
+})
---
+++ routes/routes.js
@@ -0,0 +1,151 @@
+const express = require("express");
+const router = express.Router();
+const Users = require("../models/users");
+const multer = require("multer");
+const fs = require("fs");
+
+//image upload
+var storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,"./uploads");
+    },
+    filename : function(req,file,cb){
+        cb(null, file.fieldname+"_"+Date.now()+"_"+file.originalname);
+    }
+});
+
+var upload = multer({
+    storage : storage,
+}).single("image");
+
+router.get("/",async (req,res)=>{
+    try{
+        const users = await Users.find();
+        console.log(users.length);
+        res.render("index",{
+            title : "My page",
+            users : users
+        });
+    }
+    catch(err){
+        req.session.message = {
+            type: "error",
+            message: "Error in fatching data"
+        }
+        res.render("index",{
+            title : "My page",
+        });
+    }
+    
+});
+
+router.get("/add",(req,res)=>{
+    res.render("add_users",{
+        title : "Add Users"
+    });
+});
+
+router.post("/add", upload, async (req,res)=>{
+    console.log("debugging", req.body);
+    const user = new Users({
+        name : req.body.name,
+        email : req.body.email,
+        phone : req.body.phone,
+        image : req.file.filename
+    });
+
+    try {
+        await user.save();
+        req.session.message = {
+            type: "success",
+            message: "User added successfully!"
+        };
+        res.redirect("/");
+    } catch (err) {
+        res.json({ message: "Error while adding a user", type: "danger" });
+        res.status(401);
+    }
+});
+
+router.get("/edit/:id", async(req,res)=>{
+    try{
+        const id = req.params.id;
+        console.log("id : ", id);
+        const user = await Users.findById(id);
+        console.log("name : ", user.name);
+        res.render("edit_user",{
+            title : "Edit User",
+            user : user
+        });
+    }
+    catch(err){
+        res.json({ message: "Error finding a user", type: "danger" });
+        res.status(401);
+    }
+})
+
+router.get("/delete/:id",async (req,res)=>{
+    try{
+        const id = req.params.id;
+        console.log("debug", id);
+        const user = await Users.findById(id);
+        console.log("debug",user);
+        if(user){
+            await Users.findByIdAndDelete(id);
+            console.log("debug after delete");
+            req.session.message = {
+                type: "success",
+                message: "User has been deleted successfully!"
+            };
+            res.redirect("/");
+        }
+    }
+    catch(err){
+        res.json({ message: "Error while deleting a user", type: "danger" });
+        res.status(401);
+    }
+})
+
+router.post("/update/:id", upload, async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const user = await Users.findById(id);
+        let newImage = "";
+        if(user){
+            if(req.file){
+                newImage = req.file.filename;
+                try{
+                    fs.unlinkSync("./uploads/"+req.body.old_image);
+                }
+                catch(err){
+                    console.log(err);
+                }
+            }
+            else{
+                newImage = req.body.old_image;
+            }
+
+            await Users.findByIdAndUpdate(id,{
+                name : req.body.name,
+                email : req.body.email,
+                phone : req.body.phone,
+                image : newImage
+            });
+
+            req.session.message = {
+                type: "success",
+                message: "User updated successfully!"
+            };
+            res.redirect("/");
+        }
+    }
+    catch(err){
+        req.session.message = {
+            type: "danger",
+            message: "User not updated!"
+        };
+        res.redirect("/");
+    }
+})
+
+module.exports = router;
